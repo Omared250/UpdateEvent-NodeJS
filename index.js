d3c30ed813e83cadf1bbc0e8b61683684d1cf491
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-06-12 17:13:05 +0200

    listening petitions

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+const express = require('express');
+// const { dbConnection } = require('./dataBase/config');
+// require('dotenv').config();
 
 
+// create express server
+const app = express();
 
-console.log('Hola mundo!qwqwqw!');
+// DataBase
+// dbConnection();
+
+// Public Directory
+// app.use( express.static('public') );
+
+// read and parse body
+// app.use( express.json() );
+
+// routes
+// app.use('/api/events', require('./routes/events'));
+app.get('/', (re, res) => {
+    console.log('/ required');
+    res.json({
+        ok: true
+    });
+})
+
+
+// Listening
+app.listen( 4002, () => {
+    console.log(`Server running on port ${ 4002 }`);
+} )
